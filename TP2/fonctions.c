3f0827896d529859886485bd7e288ea1cad9670a
PaulCarlu <paulcarlu2908@gmail.com>
2026-02-06 14:44:54 +0100

    ajout des fonctions du TP2-a

--- TP2/fonctions.c
+++ TP2/fonctions.c
@@ -46,5 +46,7 @@ void transFK(int q, float **a, float *Fbase, float *FK) {
 }
 
 void selectPts(int nb, int num[], float *coorEns[], float *coorSel[]) {
-
+	for (int i=0; i<nb; i++) {
+		coorSel[i] = coorEns[num[i]-1];
+	}
 }
