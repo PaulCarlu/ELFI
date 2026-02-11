f653a0f8e3c28a0fba67cb21508397ee4729c52a
PaulCarlu <paulcarlu2908@gmail.com>
2026-02-11 16:29:32 +0100

    Ajout des fonctions pour le calcul des intégrales élémentaires

---
+++ TP2/integralesElementaires.c
@@ -0,0 +1,58 @@
+// Carlu Paul - Martin Malo
+//
+#include <stdlib.h>
+#include <stdio.h>
+
+void W(int nbneel, float *Fbase, float eltdif, float cofvar, float *vecelm) {
+    /*
+    Calcul de 
+        A00*Fbase_i*Fbase_j
+    et 
+        BN*Fbase_i*Fbase_j
+    où A00 et BN sont rerésentés par cofvar
+    
+    En entrée :
+    - nbneel : nombre de noeuds de l'élément
+    - Fbase : vecteur des valeurs des fontions de base au point de quadrature courant
+    - eltdif : élément différentiel multiplié par le poids de quadrature
+    - cofvar : valeur du coefficient variable (fonction à intégrer calculée en l'image
+               par FK du point de quadrature courant)
+    - vecelm : vecteur élémentaire de masse à actualiser
+    En sortie :
+    - vecelm : vecteur élémentaire de masse actualisée
+    */
+    for (int i=0; i<nbneel; i++) {
+        vecelm[i] = vecelm[i] + eltdif*cofvar*Fbase[i];
+    }
+}
+
+void WW(int nbneel, float *Fbase, float eltdif, float cofvar, float **matelm) {
+    /*
+    Calcul de 
+        A00*Fbase_i*Fbase_j
+    et 
+        BN*Fbase_i*Fbase_j
+    où A00 et BN sont rerésentés par cofvar
+    
+    En entrée :
+    - nbneel : nombre de noeuds de l'élément
+    - Fbase : vecteur des valeurs des fontions de base au point de quadrature courant
+    - eltdif : élément différentiel multiplié par le poids de quadrature
+    - cofvar : valeur du coefficient variable (fonction à intégrer calculée en l'image
+               par FK du point de quadrature courant)
+    - matelm : matrice élémentaire de masse à actualiser
+    En sortie :
+    - matelm : matrice élémentaire de masse actualisée
+    */
+    float coeff;
+    for (int i=0; i<nbneel; i++) {
+        coeff = eltdif*cofvar*Fbase[i];
+        for (int j=0; j<nbneel; j++) {
+            matelm[i][j] = matelm[i][j] + coeff*Fbase[j];
+        }
+    }
+}
+
+void ADWDW() {
+    
+}
