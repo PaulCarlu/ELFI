9850d88f346728269d5cc9a068efc5addbdd9c1c
malomartin79-ai <malomartin79@gmail.com>
2026-02-11 18:00:42 +0100

    test

--- TP2/fonctionsUtilitaires.h
+++ TP2/fonctionsUtilitaires.h
@@ -6,6 +6,6 @@ void calFbase(int,float*,float*);
 void calDerFbase(int,float*,float**);
 void transFK(int,float**,float*,float*);
 void matJacob(int,int,float**,float**,float**);
-void invertM2x2(float**,float**,float);
+void invertM2x2(float**,float**,float*);
 void numNaret(int,int,int*);
-void selectPts(int,int,float*,float*);
+void selectPts(int,int*,float**,float**);
