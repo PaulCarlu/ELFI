667c70ff5acd6b7c80835b86cb54a5ecef3ca7cb
PaulCarlu <paulcarlu2908@gmail.com>
2026-02-18 15:40:58 +0100

    Ajout de ADWDW

--- TP2/integralesElementaires.c
+++ TP2/integralesElementaires.c
@@ -53,6 +53,29 @@ void WW(int nbneel, float *Fbase, float eltdif, float cofvar, float **matelm) {
     }
 }
 
-void ADWDW() {
-    
+void ADWDW(int nbneel, float **DerParFbase, float eltdif, float **cofvar, float **matelm) {
+    /*
+    Calcul de 
+        somme sur alpha,beta : 
+            cofvar[alpha][beta] * dérivé Fbase[i] par rapport à alpha * dérivé Fbase[j] par rapport à beta
+
+    En entrée :
+    - nbneel : nombre de noeuds de l'élément
+    - DerParFbase : matrice des coefficients : transpose((grad Fbase[i](xk))) * colonne alpha(inverse(JacFK(xk)))
+                    où alpha=1,2 ; xk est un point de quadrature et i varie selon le nombre de noeud sur l'élément de référence
+    - eltdif : élément différentiel multiplié par le poids de quadrature
+    - cofvar : matrice des coefficients variables (fonction à intégrer calculée en l'image
+               par FK du point de quadrature courant)
+    - matelm : matrice élémentaire de masse à actualiser
+    En sortie :
+    - matelm : matrice élémentaire de masse actualisée
+    */
+    for (int i=0; i<nbneel; i++) {
+        for (int j=0; j<nbneel; j++) {
+            matelm[i][j] = matelm[i][j] + cofvar[0][0]*DerParFbase[i][0]*DerParFbase[j][0]*eltdif;
+            matelm[i][j] = matelm[i][j] + cofvar[1][0]*DerParFbase[i][1]*DerParFbase[j][0]*eltdif;
+            matelm[i][j] = matelm[i][j] + cofvar[0][1]*DerParFbase[i][0]*DerParFbase[j][1]*eltdif;
+            matelm[i][j] = matelm[i][j] + cofvar[1][1]*DerParFbase[i][1]*DerParFbase[j][1]*eltdif;
+        }
+    }
 }
