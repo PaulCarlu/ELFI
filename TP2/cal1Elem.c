20a92908772861ebb9d42a26009e754d0b38f04b
PaulCarlu <paulcarlu2908@gmail.com>
2026-03-18 14:01:11 +0100

    correction de 2 3 trucs

--- TP2/cal1Elem.c
+++ TP2/cal1Elem.c
@@ -34,12 +34,11 @@ void cal1Elem(int nbneel, int nRefDom, int nbRefD0, int* numRefD0, int nbRefD1, 
     NuDElem[i] = 1;
     uDElem[i] = 0;
   }
-  
-  
-  int numPnts_Naret[2];
-  
-  
 
+  int numPnts_Naret[2];
+  float **mataret = allocmatFLOAT(2,2);
+  float vecaret[2];
+  float **coorAr = allocmatFLOAT(2,2);
 
   /* Boucle sur les arêtes de l'élément courant */
   for(int i=0;i<nbaret;i++){
@@ -63,36 +62,27 @@ void cal1Elem(int nbneel, int nRefDom, int nbRefD0, int* numRefD0, int nbRefD1, 
     // Calcul avec intAret sur les arêtes avec condition de Neumann/Fourier
     for(int j=0; j<nbRefF1; j++) {
 
-      float **mataret = allocmatFLOAT(2,2);
-      float vecaret[2];
-      for(int n=0; n<2;n++) {
-        mataret[n][0] = 0;
-        mataret[n][1] = 0;
-        vecaret[n] = 0;
-      }
-
       if(nRefArEl[i] == numRefF1[j]){
-        float **coorAr = allocmatFLOAT(2,2);
-        selectPts(2,numPnts_Naret,coorEl,coorAr); // A voir si jamais avec le prof pour la façon dont est fait numNaret
+        for(int n=0; n<2;n++) {
+          mataret[n][0] = 0;
+          mataret[n][1] = 0;
+          vecaret[n] = 0;
+        }
+        selectPts(2,numPnts_Naret,coorEl,coorAr);
         intAret(coorAr,mataret,vecaret);
 
-        
         for(int k=0;k<2;k++){
-          printf("\n");
           for(int l=0;l<2;l++){
-            printf(" vecaret[%d] = %f",l,vecaret[l]);
             MatElem[numPnts_Naret[k]-1][numPnts_Naret[l]-1] =  MatElem[numPnts_Naret[k]-1][numPnts_Naret[l]-1] + mataret[k][l];
-    
           }
           SMbrElem[numPnts_Naret[k]-1] += vecaret[k];
         }
-
-       
       }
-      freematFLOAT(mataret);
+      
     }
   }
   freematFLOAT(matelm);
+  freematFLOAT(mataret);
 }
 
 void impCalEl(int K, int typEl, int nbneel, float **MatElem, float *SMbrElem,
