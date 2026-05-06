7eda4e799b7ed8053e3e2e7ecd89f47d1dc1b93f
PaulCarlu <paulcarlu2908@gmail.com>
2026-05-06 17:56:10 +0200

    .

--- TP5/main5.c
+++ TP5/main5.c
@@ -2,6 +2,7 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "../TP1/lecfima.h"
 #include "../TP1/allocmat.h"
 #include "../TP3/Assemblage.h"
@@ -10,128 +11,245 @@
 #include "dSMOaPR.h"
 #include "CalSol.h"
 
-
-//extern int nucas;
+int Domaine = 0;
+int nucas = 0;
 //extern float* a,b,c,d;  
 
 int main() {
-    // Utilisation de lecfima pour charger le fichier de maillage
+    // Demande des choix de l'utilisateur
+    printf("Choisir le domaine d'étude (1 ou 2): ");
+    scanf("%d",&Domaine);
+    while(Domaine != 1 && Domaine != 2 ) {
+        printf("Numéro de Domaine invalide (choisir 1 ou 2) : ");
+        scanf("%d",&Domaine);
+    }
+    printf("Choisir le cas (1, 2 ou 3): ");
+    scanf("%d",&nucas);
+    while(nucas != 1 && nucas != 2 && nucas != 3) {
+        printf("Numéro de cas invalide (choisir entre 1, 2 ou 3) : ");
+        scanf("%d",&nucas);
+    }
+    int maillage = 0;
+    printf("Choix du maillage (quadrangle : 1 | triangle : 2) : ");
+    scanf("%d",&maillage);
+    while(maillage != 1 && maillage != 2 ) {
+        printf("Choix de maillage invalide (choisir 1 ou 2) : ");
+        scanf("%d",&maillage);
+    }
+    char ficmai[30];
+    int nbneel = 0;
+    if (maillage == 1) {
+        nbneel = 4;
+    } else {
+        nbneel = 3;
+    }
+
+    int IMPFCH = 1;
+    printf("Choisir le 'n' dans fort.n, le fichier dans lequel apparaît les erreurs : ");
+    scanf("%d",&IMPFCH);
+
+    // Initialisation de toutes les variables pour la boucle for
     /* Notations :
-    - typEl : type de l'élément géométrique
-    - nbtng : nombre total de noeuds
-    - nbtel : nombre total d'éléments
-    - nbneel : nombre de noeuds sur un élément
-    - nbaret : nombre d'arêtes sur un élément
-    - coord : matrice de toutes les coordonnées
-    - ngnel : matrice des numéros globaux des éléments
-    - nRefAr : matrice des numéros de référence associés aux arrêtes
+        - typEl : type de l'élément géométrique
+        - nbtng : nombre total de noeuds
+        - nbtel : nombre total d'éléments
+        - nbneel : nombre de noeuds sur un élément
+        - nbaret : nombre d'arêtes sur un élément
+        - coord : matrice de toutes les coordonnées
+        - ngnel : matrice des numéros globaux des éléments
+        - nRefAr : matrice des numéros de référence associés aux arrêtes
     */
-    char *ficmai = "../Maillages/car3x3t_3";
-    int typEl, nbtng, nbtel, nbneel, nbaret;
+    int typEl, nbtng, nbtel, nbaret;
     float **coord; 
     int **ngnel, **nRefAr;
-    
-    
-    lecfima(ficmai,&typEl,&nbtng,&coord,&nbtel,&ngnel,&nbneel,&nbaret,&nRefAr);
 
-    // Initialisation des variables
     /* Notations :
-    - nRefDom : numéro de référence des arêtes internes du domaine
-    - nbRefD0, nbRefD1, nbRefF1 : nombre de valeurs contenues dans les différents vecteurs numRefD0, numRefD1 et numRefF1
-    - numRefD0, numRefD1, numRefF1 : vecteurs des numéros de référence de chaque type
-    ---> numRefD0 : Dirichlet homogène ; numRefD1 : Dirichlet non homogène ; numRefF1 : Fourier/Neumann
-    - coorEl : coordonnées de l'élement sélectionné
-    - nRefArEl : matrice des numéros de référence associés aux arrêtes de l'élement sélectionné
-    - MatElem : matrice élémentaire
-    - SMbrElem : vecteur du second membre élémentaire
-    - NuDElem : vecteur des noeuds porteurs d'une condition de Dirichlet
-    - uDElem : vecteurs des valeurs portées par ces noeuds avec une condition de Dirichlet
+        - nRefDom : numéro de référence des arêtes internes du domaine
+        - nbRefD0, nbRefD1, nbRefF1 : nombre de valeurs contenues dans les différents vecteurs numRefD0, numRefD1 et numRefF1
+        - numRefD0, numRefD1, numRefF1 : vecteurs des numéros de référence de chaque type
+        ---> numRefD0 : Dirichlet homogène ; numRefD1 : Dirichlet non homogène ; numRefF1 : Fourier/Neumann
+        - coorEl : coordonnées de l'élement sélectionné
+        - nRefArEl : matrice des numéros de référence associés aux arrêtes de l'élement sélectionné
+        - MatElem : matrice élémentaire
+        - SMbrElem : vecteur du second membre élémentaire
+        - NuDElem : vecteur des noeuds porteurs d'une condition de Dirichlet
+        - uDElem : vecteurs des valeurs portées par ces noeuds avec une condition de Dirichlet
     */
-    float **MatElem = allocmatFLOAT(nbneel,nbneel);
-    float **coorEl = allocmatFLOAT(nbneel,2);
+    float **MatElem, **coorEl;
     float SMbrElem[nbneel], uDElem[nbneel];
     int NuDElem[nbneel];
 
-    // Conditons aux bords
-    int nRefDom = nRefAr[0][0];
-    int nbRefD0 = 1; int numRefD0[] = {nRefAr[0][nbneel-1]};
-    int nbRefD1 = 1; int numRefD1[] = {nRefAr[0][nbneel-2]};
-    int nbRefF1 = 2; int numRefF1[] = {nRefAr[nbtel-1][nbneel-2], nRefAr[nbtel-1][nbneel-1]};
+    int nRefDom;
+    int nbRefD0, nbRefF1, nbRefD1;
+    int numRefD0[4], numRefD1[4], numRefF1[4];
 
-    
-    int NbLign = nbtng; // Nombre de noeuds géométriques
-    int nbcoef = nbtng*2*((typEl==1)?8:6);
-    int* NumDLDir = malloc(NbLign*sizeof(int));
-    float* ValDLDir = calloc(NbLign,sizeof(float));
-    float* SecMembre = calloc(NbLign,sizeof(float));
-    int* AdPrCoefLi = calloc(NbLign,sizeof(int));
-    float* MatriceD = calloc((NbLign+nbcoef),sizeof(float));
-    int* NumColD = calloc(nbcoef,sizeof(int));
-    int* AdSuccLi = malloc(nbcoef*sizeof(int));
-    
-    Assemblage(typEl,nbtng,nbtel,nbneel,nbaret,nRefAr,ngnel,coord,MatElem,coorEl,SMbrElem,NuDElem,uDElem,nRefDom,
-                nbRefD0,numRefD0,nbRefD1,numRefD1,nbRefF1,numRefF1,NumDLDir,ValDLDir,SecMembre,MatriceD,AdPrCoefLi,
-                NumColD,AdSuccLi );
+    // Assemblage de la matrice
+    int NbLign, nbcoef;
+    int *NumDLDir, *AdPrCoefLi, *NumColD, *AdSuccLi;
+    float *ValDLDir, *SecMembre, *MatriceD;
 
-    //affsmd_(&NbLign,AdPrCoefLi,NumColD,AdSuccLi,MatriceD,SecMembre,NumDLDir,ValDLDir);
-    
-    nbcoef = AdPrCoefLi[NbLign-1]-1;
-    int* NumColO = malloc(nbcoef*sizeof(int));
-    float* MatriceO = calloc(NbLign+nbcoef,sizeof(float));
+    int* NumColO;
+    float* MatriceO;
 
-    dSMDaSMO(NbLign,AdPrCoefLi,NumColD,AdSuccLi,MatriceD,SecMembre,NumDLDir,ValDLDir,MatriceO,NumColO);
+    int dimProf;
+    float* MatProf;
+    int* Profil;
 
-    //affsmo_(&NbLign,AdPrCoefLi,NumColO,MatriceO,SecMembre);
-
-    int dimProf = 0;
-    for (int i=0; i<NbLign-1; i++) {
-        if (AdPrCoefLi[i+1]-AdPrCoefLi[i]!=0){
-            dimProf = dimProf + i+2 - NumColO[AdPrCoefLi[i]-1];
-        }
-    }
-
-
-    float* MatProf = calloc(NbLign+dimProf,sizeof(float));
-    int* Profil = malloc((NbLign)*sizeof(int));
-
-    dSMOaPR(NbLign,AdPrCoefLi,NumColO,MatriceO,Profil,MatProf);
-    
-   
-
-    /* Décomposition LLT
-    Résolution :extern int nucas = 1;  
-        L*Y = SecMembre
-        LT*U = Y
-    avec Y = LT*U
-    */
-    float* LowMatProf = MatProf + NbLign;
     float eps = (float) 1.0e-07;
-    float* ld = malloc(NbLign*sizeof(float));
-    float* ll = malloc(dimProf*sizeof(float));
-    float* y = malloc(NbLign*sizeof(float));
-    float* U = malloc(NbLign*sizeof(float));
+    float *LowMatProf, *ld, *ll, *y, *U;
 
-    ltlpr_(&NbLign,Profil,MatProf,LowMatProf,&eps,ld,ll);
+    float* UEX;
+
+    // Boucle sur tous les fichiers du cas considéré
+    for (int i=1; i<7; i++) {
+        if (Domaine == 1) {
+            if (maillage == 1) {
+                sprintf(ficmai,"../maillage/d1q1/d1q1_%d",(int) pow(2,i));
+            } else {
+                sprintf(ficmai,"../maillage/d1t1/d1t1_%d",(int) pow(2,i));
+            }
+        } else {
+            if (maillage == 1) {
+                sprintf(ficmai,"../maillage/d2q1/d2q1_%d",(int) pow(2,i));
+            } else {
+                sprintf(ficmai,"../maillage/d2t1/d2t1_%d",(int) pow(2,i));
+            }
+        }
+        printf("%s\n",ficmai);
+        
+        lecfima(ficmai,&typEl,&nbtng,&coord,&nbtel,&ngnel,&nbneel,&nbaret,&nRefAr);
+
+        // Conditons aux bords
+        /* Pour aller chercher les bords :
+        - bord 1 = nRefAr[0][nbneel-1]
+        - bord 2 = nRefAr[0][nbneel-2]
+        - bord 3 = nRefAr[nbtel-1][nbneel-2]
+        - bord 4 = nRefAr[nbtel-1][nbneel-1]
+        */
+       nRefDom = nRefAr[0][0];
+        if (Domaine == 1) {
+            switch (nucas)
+            {
+            case 1 :
+                nbRefD0 = 4, nbRefD1 = 0, nbRefF1 = 0; 
+                numRefD0[0] = nRefAr[0][nbneel-1];
+                numRefD0[1] = nRefAr[0][nbneel-2];
+                numRefD0[2] = nRefAr[nbtel-1][nbneel-2]; 
+                numRefD0[3] = nRefAr[nbtel-1][nbneel-1];
+                break;
+            case 2 :
+                nbRefD0 = 4, nbRefD1 = 0, nbRefF1 = 0; 
+                numRefD0[0] = nRefAr[0][nbneel-1];
+                numRefD0[1] = nRefAr[0][nbneel-2];
+                numRefD0[2] = nRefAr[nbtel-1][nbneel-2]; 
+                numRefD0[3] = nRefAr[nbtel-1][nbneel-1];
+                break;
+            case 3 :
+                nbRefD0 = 0, nbRefD1 = 0, nbRefF1 = 4; 
+                numRefF1[0] = nRefAr[0][nbneel-1];
+                numRefF1[1] = nRefAr[0][nbneel-2];
+                numRefF1[2] = nRefAr[nbtel-1][nbneel-2]; 
+                numRefF1[3] = nRefAr[nbtel-1][nbneel-1];
+                break;
+            default:
+                break;
+            }
+        } else {
+            switch (nucas)
+            {
+            case 1 :
+                nbRefD0 = 4, nbRefD1 = 0, nbRefF1 = 0; 
+                numRefD0[0] = nRefAr[0][nbneel-1];
+                numRefD0[1] = nRefAr[0][nbneel-2];
+                numRefD0[2] = nRefAr[nbtel-1][nbneel-2]; 
+                numRefD0[3] = nRefAr[nbtel-1][nbneel-1];
+                break;
+            case 2 :
+                nbRefD0 = 4, nbRefD1 = 0, nbRefF1 = 0; 
+                numRefD0[0] = nRefAr[0][nbneel-1];
+                numRefD0[1] = nRefAr[0][nbneel-2];
+                numRefD0[2] = nRefAr[nbtel-1][nbneel-2]; 
+                numRefD0[3] = nRefAr[nbtel-1][nbneel-1];
+                break;
+            case 3 :
+                nbRefD0 = 2, nbRefD1 = 0, nbRefF1 = 2; 
+                numRefD0[0] = nRefAr[0][nbneel-1];
+                numRefD0[1] = nRefAr[nbtel-1][nbneel-1];
+                numRefF1[0] = nRefAr[0][nbneel-2];
+                numRefF1[1] = nRefAr[nbtel-1][nbneel-2];
+                break;
+            default:
+                break;
+            }
+        }
+        MatElem = allocmatFLOAT(nbneel,nbneel);
+        coorEl = allocmatFLOAT(nbneel,2);
+        
+        // Assemblage de la matrice
+        NbLign = nbtng; // Nombre de noeuds géométriques
+        nbcoef = nbtng*2*((typEl==1)?8:6);
+        NumDLDir = malloc(NbLign*sizeof(int));
+        ValDLDir = calloc(NbLign,sizeof(float));
+        SecMembre = calloc(NbLign,sizeof(float));
+        AdPrCoefLi = calloc(NbLign,sizeof(int));
+        MatriceD = calloc((NbLign+nbcoef),sizeof(float));
+        NumColD = calloc(nbcoef,sizeof(int));
+        AdSuccLi = malloc(nbcoef*sizeof(int));
+        
+        Assemblage(typEl,nbtng,nbtel,nbneel,nbaret,nRefAr,ngnel,coord,MatElem,coorEl,SMbrElem,NuDElem,uDElem,nRefDom,
+                    nbRefD0,numRefD0,nbRefD1,numRefD1,nbRefF1,numRefF1,NumDLDir,ValDLDir,SecMembre,MatriceD,AdPrCoefLi,
+                    NumColD,AdSuccLi );
+
+        //affsmd_(&NbLign,AdPrCoefLi,NumColD,AdSuccLi,MatriceD,SecMembre,NumDLDir,ValDLDir);
+        
+        nbcoef = AdPrCoefLi[NbLign-1]-1;
+        NumColO = malloc(nbcoef*sizeof(int));
+        MatriceO = calloc(NbLign+nbcoef,sizeof(float));
+
+        dSMDaSMO(NbLign,AdPrCoefLi,NumColD,AdSuccLi,MatriceD,SecMembre,NumDLDir,ValDLDir,MatriceO,NumColO);
+
+        //affsmo_(&NbLign,AdPrCoefLi,NumColO,MatriceO,SecMembre);
+
+        dimProf = 0;
+        for (int i=0; i<NbLign-1; i++) {
+            if (AdPrCoefLi[i+1]-AdPrCoefLi[i]!=0){
+                dimProf = dimProf + i+2 - NumColO[AdPrCoefLi[i]-1];
+            }
+        }
+
+        MatProf = calloc(NbLign+dimProf,sizeof(float));
+        Profil = malloc((NbLign)*sizeof(int));
+
+        dSMOaPR(NbLign,AdPrCoefLi,NumColO,MatriceO,Profil,MatProf);
+        
+        /* Décomposition LLT
+        Résolution :extern int nucas = 1;  
+            L*Y = SecMembre
+            LT*U = Y
+        avec Y = LT*U
+        */
+        LowMatProf = MatProf + NbLign;
+        ld = malloc(NbLign*sizeof(float));
+        ll = malloc(dimProf*sizeof(float));
+        y = malloc(NbLign*sizeof(float));
+        U = malloc(NbLign*sizeof(float));
+
+        ltlpr_(&NbLign,Profil,MatProf,LowMatProf,&eps,ld,ll);
+        rsprl_(&NbLign,Profil,ld,ll,SecMembre,y);
+        rspru_(&NbLign,Profil,ld,ll,y,U);
     
+        // Calcul de la solution exacte
+        UEX = malloc(NbLign*sizeof(float));
+        CalSol(NbLign,coord,UEX);
 
-    rsprl_(&NbLign,Profil,ld,ll,SecMembre,y);
-    rspru_(&NbLign,Profil,ld,ll,y,U);
-   
-    // Calcul de la solution exacte
-    float* UEX = malloc(NbLign*sizeof(float));
-    CalSol(NbLign,coord,UEX);
-
-    for(int i=0; i<NbLign; i++){
-        printf("U[%d] = %.15f ; UEX[%d] = %.15f\n",i,U[i],i,UEX[i]);
+        /*
+        for(int i=0; i<NbLign; i++){
+            printf("U[%d] = %.15f ; UEX[%d] = %.15f\n",i,U[i],i,UEX[i]);
+        }
+        */
+        
+        affsol_(&NbLign,coord[0],U,UEX,&IMPFCH);
     }
-    
-
-    int IMPFCH = -10;
-    affsol_(&NbLign,coord[0],U,UEX,&IMPFCH);
-    printf("\nOK\n");
-    printf("OK\n");
-    printf("OK\n");
-    printf("OK\n");
 
     // Free des vecteurs et matrices
     freematFLOAT(MatElem);
@@ -149,7 +267,6 @@ int main() {
     free(NumColO);
     free(MatProf);
     free(Profil);
-    free(LowMatProf);
     free(ld);
     free(ll);
     free(y);
